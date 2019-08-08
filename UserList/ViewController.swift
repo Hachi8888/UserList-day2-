a0f29bd02dcd7c7cf4207af198c5da96534a9d82
Hachi8888 <h.nana.420@gmail.com>
2019-08-08 17:35:09 +0900

    first commit

---
+++ UserList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserList
+//
+//  Created by VERTEX22 on 2019/08/08.
+//  Copyright © 2019 N-project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
